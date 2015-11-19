6963986e8f670244b9ec8f7b64ad52f2f57932f1
williamgong2015 <williamgong1992@gmail.com>
2015-11-18 22:53:00 -0500

    ringbuffer

--- src/util/RingBuffer.java
+++ src/util/RingBuffer.java
@@ -1,36 +1,172 @@
 package util;
 
+import java.lang.reflect.Array;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Generic RingBuffer implements {@code Collection}, {@code Queue} and 
+ * {@code BlockingQueue} using RingBufferLock
+ *
+ */
 public class RingBuffer<E> implements Collection<E>, Queue<E>, BlockingQueue<E>{
 	
-	private E[] data;
-	private int head;
-	private int tail;
+	public E[] data;
+	private int head = 0;
+	private int tail = 0;
+	private int modCount;
+	private int size;
+	private RingBufferLock lock = new RingBufferLock();
 	
+	/**
+	 * Inner lock class for RingBuffer to enforce isolation and enhance 
+	 * concurrency
+	 */
+	public class RingBufferLock {
+		
+		int reading = 0;           // how many different readers are reading it
+		int remove_held_count = 0; // number of times held by current remover
+		int writer_held_count = 0; // number of times held by current writer
+		Thread writer = null;      // null if not locked by a writer
+		Thread remover = null;     // null if not locked by a remover
+		Lock rmLock = new RemoveLock();
+		Lock wrLock = new WriteLock();
+		Lock rdLock = new ReadLock();
+		
+		/**
+		 * Lock required for reading from the {@code RingBuffer}
+		 * Can have multiple concurrent readers, 
+		 * but can't read together with writer and remover
+		 */
+		class ReadLock implements Lock{
+
+			@Override
+			public void lock() {
+				synchronized (RingBufferLock.this) {
+					// can't read while someone is removing or writing it
+					while (remover != null || ( writer != null && head == tail)) { 
+						try {
+							RingBufferLock.this.wait();
+						} catch (Exception e) {} }
+					// one more reader
+					reading++;
+				}	
+			}
+
+			@Override
+			public void unlock() {
+				synchronized (RingBufferLock.this) {
+					reading--;
+					RingBufferLock.this.notifyAll();
+				}
+			}
+			
+		}
+		
+		/**
+		 * Lock required for removing from the {@code RingBuffer}
+		 * Can
+		 *
+		 */
+		class RemoveLock implements Lock {
+			public void lock() {
+				Thread me = Thread.currentThread();
+				synchronized (RingBufferLock.this) {
+					// already holding the lock
+					if (remover == me) { remove_held_count++; return; } 
+					// can't remove while someone is reading 
+					while (reading != 0 || remover != null || tail == head ) { 
+						try {
+							RingBufferLock.this.wait();
+						} catch (Exception e) {}
+					}
+					remover = me;
+					remove_held_count = 1;
+				}	
+			}
+
+			@Override
+			public void unlock() {
+				synchronized (RingBufferLock.this) {
+					remove_held_count--;
+					if (remove_held_count > 0) return; // still holding it!
+					remover = null;
+					head = (head+1)%size;
+					RingBufferLock.this.notifyAll();
+				}
+			}
+		}
+
+		public class WriteLock implements Lock {
+			public void lock() {
+				Thread me = Thread.currentThread();
+				synchronized (RingBufferLock.this) {
+					if (writer == me) { writer_held_count++; return; } // already holding the lock
+
+					while (writer != null || reading != 0 || (tail + size + 1)%size == head) { 
+						try {
+							RingBufferLock.this.wait();
+						} catch (Exception e) {}
+					}
+					writer = me;
+					writer_held_count = 1;
+				}
+			}
+			public void unlock() {
+				synchronized (RingBufferLock.this) {
+					writer_held_count--;
+					if (writer_held_count > 0) return; // still holding it!
+					writer = null;
+					tail = (tail + 1)%size;
+					RingBufferLock.this.notifyAll();
+				}
+			}
+		}
+	}
+
 	@SuppressWarnings("unchecked")
-	public RingBuffer(int size) {
-		head = 0;
-		tail = 0;
-		data = (E[])(new Object[size]);
+	public RingBuffer(Class<E> clazz, int size) {
+		modCount = 0;
+		if(size < 0)
+			throw new IllegalArgumentException("Illegal Capacity: "+size);
+		data = (E[]) Array.newInstance(clazz, size);
+		this.size = size;
 	}
 	
+	/**
+	 * Returns the number of elements in this collection. 
+	 * Override from {@code Collection}
+	 * @return number of the elements in this collection
+	 */
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub  1
-		return 0;
+		lock.rdLock.lock();
+		int result = (tail - head) >= 0 ?  (tail - head) : (tail - head + size);
+		lock.rdLock.unlock();
+		return result;
 	}
 
+	/**
+	 * Returns true if this collection contains no elements.
+	 * Returns false otherwise
+	 * Override from {@code Collection}
+	 */
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub  1
-		return false;
+		lock.rdLock.lock();
+		boolean result = head == tail;
+		lock.rdLock.unlock();
+		return result;
 	}
 
+	/**
+	 * 
+	 * @param o
+	 * @return
+	 */
 	@Override
 	public boolean contains(Object o) {
 		// TODO Auto-generated method stub   1
@@ -45,8 +181,12 @@ public class RingBuffer<E> implements Collection<E>, Queue<E>, BlockingQueue<E>{
 
 	@Override
 	public boolean add(E e) {
-		// TODO Auto-generated method stub  1
-		return false;
+		if(this.size() == size)
+			throw new IllegalStateException();
+		lock.wrLock.lock();
+		data[tail] = e;
+		lock.wrLock.unlock();
+		return true;
 	}
 
 	@Override
@@ -73,22 +213,44 @@ public class RingBuffer<E> implements Collection<E>, Queue<E>, BlockingQueue<E>{
 		return null;
 	}
 
+	/**
+	 * Retrieves, but does not remove, the head of this queue, 
+	 * or returns null if this queue is empty.
+	 * Override from {@code Queue}
+	 */
 	@Override
 	public E peek() {
-		// TODO Auto-generated method stub
-		return null;
+		lock.rdLock.lock();
+		if(this.size == 0) {
+			lock.rdLock.unlock();
+			return null;
+		}
+		E temp = data[head];
+		lock.rdLock.unlock();
+		return temp;
 	}
 
+	/**
+	 * Inserts the specified element into this queue, 
+	 * waiting if necessary for space to become available.
+	 * @param e
+	 * @throws InterruptedException
+	 * Override from {@code BlockingQueue}
+	 */
 	@Override
 	public void put(E e) throws InterruptedException {
-		// TODO Auto-generated method stub
-		
+		lock.wrLock.lock();
+		data[tail] = e;
+		lock.wrLock.unlock();
+		return;
 	}
 
 	@Override
 	public E take() throws InterruptedException {
-		// TODO Auto-generated method stub
-		return null;
+		lock.rmLock.lock();
+		E temp = data[head];
+		lock.rmLock.unlock();
+		return temp;
 	}
 
 	@Override
---
+++ src/util/test.java
@@ -0,0 +1,155 @@
+package util;
+
+import java.util.HashSet;
+
+public class test {
+	static RingBuffer<Integer> r = new RingBuffer<>(Integer.class, 1001);
+	static HashSet<Integer> removed = new HashSet<>();
+	
+	class TestRun implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	synchronized public static void testing() {
+			TestAddThread[] threads = new TestAddThread[10];
+			int j = 0;
+			for (int i = 0; i < 1000; i += 100) {
+				threads[j++] = new TestAddThread(r, i, i+100);
+			}
+			for (int i = 0; i < 10; ++i) {
+				threads[i].start();
+			}
+			
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+//		testing();
+//		Thread.sleep(1000);
+//		validating();
+		testing2();
+		Thread.sleep(1000);
+		check();
+	}
+	
+	synchronized static void check() {
+		HashSet<Integer> s = removed;
+		if(r.size() != 0)
+			System.out.println("FalSe");
+	}
+	
+	synchronized public static void validating() {
+		boolean[] test = new boolean[1001];
+		for(int i = 0; i < 1000; i++) {
+			int temp = (r.data[i]);
+			if(test[temp] == true)
+				System.out.println("False");
+			test[temp] = true;
+		}
+		System.out.println("True");
+	}
+	
+	
+	static class TestAddThread extends Thread {
+		final RingBuffer<Integer> r;
+		int start;
+		int end;
+		TestAddThread(RingBuffer<Integer> r, int start, int end) {
+			this.r = r;
+			this.start = start;
+			this.end = end;
+		}
+		public void run() {
+			for (int i = start; i < end; i++) {
+				r.add(i);
+			}
+		}
+	}
+	
+	static class RemoveThread extends Thread {
+		final RingBuffer<Integer> r;
+		int start;
+		int end;
+		RemoveThread(RingBuffer<Integer> r, int start, int end) {
+			this.r = r;
+			this.start = start;
+			this.end = end;
+		}
+		public void run() {
+			for (int i = start; i < end; i++) {
+				try {
+					int temp = r.take();
+					if(removed.contains(temp))
+						System.out.println("FALSE");
+					removed.add(temp);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	class TestSizeThread extends Thread {
+		final RingBuffer<Integer> r;
+		TestSizeThread(RingBuffer<Integer> r) {
+			this.r = r;
+		}
+		public void run() {
+			System.out.println("Size: " + r.size());
+		}
+	}
+	class TestIsEmptyThread extends Thread {
+		final RingBuffer<Integer> r;
+		TestIsEmptyThread(RingBuffer<Integer> r) {
+			this.r = r;
+		}
+		public void run() {
+			System.out.println("Is Empty: " + r.isEmpty());
+		}
+	}
+	class TestPeekThread extends Thread {
+		final RingBuffer<Integer> r;
+		TestPeekThread(RingBuffer<Integer> r) {
+			this.r = r;
+		}
+		public void run() {
+			System.out.println("Peek: " + r.peek());
+		}
+	}
+	class TestPutThread extends Thread {
+		final RingBuffer<Integer> r;
+		TestPutThread(RingBuffer<Integer> r) {
+			this.r = r;
+		}
+		public void run() {
+			try {
+				r.put(util.RandomGen.randomNumber());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				System.out.println("peek throw ");
+			}
+		}
+	}
+	synchronized public static void testing2() {
+		TestAddThread[] threads = new TestAddThread[10];
+		RemoveThread[] rthreads = new RemoveThread[10];
+		int j = 0;
+		for (int i = 0; i < 1000; i += 100) {
+			threads[j] = new TestAddThread(r, i, i+100);
+			rthreads[j++] = new RemoveThread(r,i,i+100);
+		}
+		for (int i = 0; i < 10; ++i) {
+			threads[i].start();
+			rthreads[i].start();
+		}
+}
+}
